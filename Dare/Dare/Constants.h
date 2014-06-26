f143ddf4b5c010cbc909ec886bc43b1e82daad6b
Dare Ryan <dareryan@me.com>
2014-06-25 20:33:52 -0400

    Add constants to gitignore

---
+++ Dare/Dare/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  Dare
+//
+//  Created by Dare Ryan on 6/25/14.
+//  Copyright (c) 2014 Dare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+@end
---
+++ Dare/Dare/Constants.m
@@ -0,0 +1,13 @@
+//
+//  Constants.m
+//  Dare
+//
+//  Created by Dare Ryan on 6/25/14.
+//  Copyright (c) 2014 Dare. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+
+@end
